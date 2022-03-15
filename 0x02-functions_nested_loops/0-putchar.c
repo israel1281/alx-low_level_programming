08f3a1ef93d641df6ffd07c64aa34c2631076233
israel1281 <kidoski1@gmail.com>
2022-03-15 10:40:26 +0000

    init commit

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - Prints 0
+ * @void: Empty parameter list for main.
+ *
+ * Description: Prints Holberton w/out stdio
+ *
+ * Return: 0 for success
+*/
+int main(void)
+{
+	_putchar('m');
+	_putchar('a');
+	_putchar('i');
+	_putchar('n');
+	_putchar('\n');
+	return (0);
+}
